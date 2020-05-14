7b35b271759d989d37930f6e93722bf0d37fea1a
todo <39897091+kamonohasikamo@users.noreply.github.com>
2020-05-15 00:35:36 +0900

    必・選必の目標入力欄の完成？と目標設定ボタンの設置、それに伴うCSSの修正->リファクタリング

--- js/tomorrowPageModel.js
+++ js/tomorrowPageModel.js
@@ -12,62 +12,71 @@ $(function() {
 	}
 });
 
-function ReqAddForm() {
-	var point = document.getElementById("inputRequired");
+var setFormInfomation = {
+	inputRequired : {
+		text : "今日の必須項目を書く",
+		form : "inputRequired"
+	},
 
-	var inputText = document.createElement("input");
-	inputText.setAttribute("type", "text");
-	inputText.setAttribute("class", "inputText");
-	inputText.setAttribute("placeholder", "今日の必須目標を書く");
-
-	point.appendChild(inputText);
-
-	var inputWeight = document.createElement("input");
-	inputWeight.setAttribute("type", "text");
-	inputWeight.setAttribute("class", "inputWeight");
-	inputWeight.setAttribute("placeholder", "重要度");
-
-	point.appendChild(inputWeight);
-}
-
-function ReqDelForm() {
-	var point = document.getElementById("inputRequired");
-	var childCount = point.childElementCount;
-	if(childCount > 2) {
-		point.removeChild(point.children[childCount - 1]);
-		point.removeChild(point.children[childCount - 2]);
+	inputSelection : {
+		text : "今日の選択必須項目を書く",
+		form : "inputSelection"
 	}
-}
-
-function SelAddForm() {
-	var point = document.getElementById("inputSelection");
-
-	var inputText = document.createElement("input");
-	inputText.setAttribute("type", "text");
-	inputText.setAttribute("class", "inputText");
-	inputText.setAttribute("placeholder", "今日の選択必須目標を書く");
-
-	point.appendChild(inputText);
-
-	var inputWeight = document.createElement("input");
-	inputWeight.setAttribute("type", "text");
-	inputWeight.setAttribute("class", "inputWeight");
-	inputWeight.setAttribute("placeholder", "重要度");
-
-	point.appendChild(inputWeight);
-}
-
-function SelDelForm() {
-	var point = document.getElementById("inputSelection");
-	var childCount = point.childElementCount;
-	if(childCount > 2) {
-		point.removeChild(point.children[childCount - 1]);
-		point.removeChild(point.children[childCount - 2]);
-	}
-}
+};
 
 function ClearForm() {
 	var point = document.getElementById("inputTodayTarget");
-
 	point.value = "";
+}
+
+
+function addForm(elementTag) {
+	var point = document.getElementById(elementTag);
+
+	var newRow = point.insertRow();
+	var newCell = newRow.insertCell();
+
+	var inputText = document.createElement("input");
+	inputText.setAttribute("type", "text");
+	inputText.setAttribute("class", "inputText");
+	inputText.setAttribute("placeholder", setFormInfomation[elementTag].text);
+
+	newCell.appendChild(inputText);
+
+	newCell = newRow.insertCell();
+	var inputWeight = document.createElement("input");
+	inputWeight.setAttribute("type", "tel");
+	inputWeight.setAttribute("class", "inputWeight");
+	inputWeight.setAttribute("placeholder", "重要度");
+
+	newCell.appendChild(inputWeight);
+
+	newCell = newRow.insertCell();
+	var addButton = document.createElement("input");
+	addButton.setAttribute("type", "button");
+	addButton.setAttribute("value", "Add");
+	addButton.setAttribute("class", "AddDelButton");
+	addButton.setAttribute("onclick", 'addForm("' + setFormInfomation[elementTag].form + '")');
+
+	newCell.appendChild(addButton);
+
+	newCell = newRow.insertCell();
+	var delButton = document.createElement("input");
+	delButton.setAttribute("type", "button");
+	delButton.setAttribute("value", "Delete");
+	delButton.setAttribute("class", "AddDelButton");
+	delButton.setAttribute("onclick", "delForm(this)");
+
+	newCell.appendChild(delButton);
+}
+
+function delForm(obj) {
+	var tr = obj.parentNode.parentNode;
+	if (tr.parentNode.rows.length > 2) {
+		tr.parentNode.deleteRow(tr.sectionRowIndex);
+	}
+}
+
+function setTomorrowTarget() {
+
 }
